b1164250c8af62af78fa56362a581b46382c9514
Federico Brinkworth <federico.brinkworth@gmail.com>
2021-07-22 15:23:29 -0300

    disable copy to clipboard

--- client/src/ContractAddress.js
+++ client/src/ContractAddress.js
@@ -18,16 +18,23 @@ const useStyles = makeStyles((theme) => ({
 const ContractAddress = (props) => {
     const classes = useStyles();
 
+    /*
+     * Deshabilite copiar al clipboard porque en un sitio http no funciona
     const handleClick = (address) => {
       navigator.clipboard.writeText(address);
     };
+    */
 
     if(props.show) {
       return (
         <div className={classes.root}>
             <br/>
-            <span>Debe agregar el token JIT a Metamask, click para copiar la dirección del contrato al portapapeles</span>
-            <Chip avatar={<Avatar>C</Avatar>} label={web3State.contract._address} clickable color="primary" onClick={handleClick.bind(null, web3State.contract._address)} />            
+            {/*
+              <span>Debe agregar el token JIT a Metamask, click para copiar la dirección del contrato al portapapeles</span>
+              <Chip avatar={<Avatar>C</Avatar>} label={web3State.contract._address} clickable color="primary" onClick={handleClick.bind(null, web3State.contract._address)} />             
+            */}
+            <span>Debe agregar el token JIT a Metamask mediante la dirección del contrato</span>
+            <Chip avatar={<Avatar>C</Avatar>} label={web3State.contract._address} color="primary" />
         </div>
       );
     } else {
